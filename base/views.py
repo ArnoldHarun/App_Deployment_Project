d562ac8db3e036efecaeb7d30a77465f0fcac2d1
ArnoldHarun <harunk3570@gmail.com>
2024-04-03 13:33:25 +0300

    login

--- base/views.py
+++ base/views.py
@@ -21,7 +21,7 @@ from .forms import PositionForm
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
-    redirect_authenticated_user = True
+    redirect_authenticated_user = False
 
     def get_success_url(self):
         return reverse_lazy('tasks')
